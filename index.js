40520b06a9d298a02a2de8d89b54b85b43d746df
Keith Showalter <kshowalter@fsec.ucf.edu>
2018-03-19 14:21:14 -0400

    Added NaN handling to display_value function.

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ f.format_value = function(value_in){
 f.display_value = function(value_in){
   var value_out = f.format_value(value_in);
 
-  if( value_out === false ){
+  if( value_out === false || isNaN(value_out) ){
     value_out = '';
   }
 
